607800ecb2a1e6f3f36831737600f57c44945309
deradhista <deradhista@gmail.com>
2023-01-16 21:29:45 +0700

    feat: input & textarea

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,11 +1,16 @@
-import { FC, InputHTMLAttributes } from "react";
+import { FC, InputHTMLAttributes, TextareaHTMLAttributes } from "react";
 
-interface Props extends InputHTMLAttributes<HTMLInputElement> {
+interface PropsInput extends InputHTMLAttributes<HTMLInputElement> {
   id: string;
   label: string;
 }
 
-export const Input: FC<Props> = ({ label, id, ...props }) => {
+interface PropsTextArea extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+  id: string;
+  label: string;
+}
+
+export const Input: FC<PropsInput> = ({ label, id, ...props }) => {
   return (
     <div className="mb-3">
       <label>{label}</label>
@@ -14,11 +19,16 @@ export const Input: FC<Props> = ({ label, id, ...props }) => {
   );
 };
 
-export const TextArea: FC<Props> = ({ label, id, ...props }) => {
+export const TextArea: FC<PropsTextArea> = ({ label, id, ...props }) => {
   return (
-    <div className="mb-3">
+    <div className="mb-3 ">
       <label>{label}</label>
-      <input id={id} className="input input-bordered w-full" {...props} />
+
+      <textarea
+        id={id}
+        className="textarea textarea-bordered w-full "
+        {...props}
+      ></textarea>
     </div>
   );
 };
